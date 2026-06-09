9303ee337e5bc2a4bb261681ac49fa25220f8b72
NikhilJ2005 <nikhiljimmythomas@gmail.com>
2026-06-09 17:03:06 +0530

    Add local code to existing repo

--- Ques15.js
+++
@@ -1,7 +0,0 @@
-//15. Merge two objects (no sum, override second)
-
-// output:- { a: 5, b: 20, c: 15 }
-
-const obj1 = { a: 10, b: 20 }, obj2 = { a: 5, c: 15 };
-const result = { ...obj1, ...obj2 };
-console.log(result); 
